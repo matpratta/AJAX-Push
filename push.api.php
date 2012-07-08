b484b7fc275c99bcb3f80c2eecb5244dd57fbfdc
Matheus Pratta <matpratta@gmail.com>
2012-07-08 19:02:38 -0300

    Added support for changing columns.

--- push.api.php
+++ push.api.php
@@ -4,12 +4,33 @@
  * Author: Matheus Pratta
  * Details: http://push.mpratta.com.br/
  *********************************************/
- 
+
+// These variables define the default columns for Push, and can be changed with the function push_change_columns.
+
+$_PUSH_CHAN = "channel";
+$_PUSH_BODY = "body";
+$_PUSH_TIME = "time";
+
 require_once("mysql.php");
+
+/* This function change the default columns from Push. You must remember to call it on every file of your script. May cause incompatibility with existing code. */
+function push_change_columns($channel_new = false, $body_new = false, $time_new = false)
+{
+	global $_PUSH_CHAN;
+	global $_PUSH_BODY;
+	global $_PUSH_TIME;
+	
+	if($channel_new) $_PUSH_CHAN = $channel_new;
+	if($body_new) $_PUSH_BODY = $body_new;
+	if($time_new) $_PUSH_TIME = $time_new;
+}
  
 /* Read all messages from '$channel' that were posted after '$last' and match '$columns' */
 function push_read($channel, $last, $desc = false, $columns = array())
 {
+	global $_PUSH_CHAN;
+	global $_PUSH_BODY;
+	global $_PUSH_TIME;
 	global $db;
 	global $db_pref;
 	global $db_tabl;
@@ -27,7 +48,7 @@ function push_read($channel, $last, $desc = false, $columns = array())
 	// Start database
 	$db = db_connect();
 	
-	$sql = "SELECT * FROM ".sanitize($db_pref.$db_tabl)." WHERE `channel`='".sanitize($channel)."' AND `time` > '".sanitize($last)."'".$extra." ORDER BY time ".$order;
+	$sql = "SELECT * FROM ".sanitize($db_pref.$db_tabl)." WHERE `".sanitize($_PUSH_CHAN)."`='".sanitize($channel)."' AND `".sanitize($_PUSH_TIME)."` > '".sanitize($last)."'".$extra." ORDER BY `".sanitize($_PUSH_TIME)."` ".$order;
 	$data = $db->prepare($sql);
 	$data->execute();
 	$data = $data->fetchAll(PDO::FETCH_ASSOC);
@@ -41,6 +62,9 @@ function push_read($channel, $last, $desc = false, $columns = array())
 /* Writes a new message and post it into '$channel', with '$message' as the body and '$columns' as the optional extra columns */
 function push_send($channel, $message, $columns = array())
 {
+	global $_PUSH_CHAN;
+	global $_PUSH_BODY;
+	global $_PUSH_TIME;
 	global $db;
 	global $db_pref;
 	global $db_tabl;
@@ -57,7 +81,7 @@ function push_send($channel, $message, $columns = array())
 	// Start database
 	$db = db_connect();
 	
-	$sql = "INSERT INTO ".sanitize($db_pref.$db_tabl)." (`channel`, `body`, `time`".$extra.") VALUES ('".sanitize($channel)."', '".sanitize($message)."', '".time()."'".$extraVal.")";
+	$sql = "INSERT INTO ".sanitize($db_pref.$db_tabl)." (`".sanitize($_PUSH_CHAN)."`, `".sanitize($_PUSH_BODY)."`, `".sanitize($_PUSH_TIME)."`".$extra.") VALUES ('".sanitize($channel)."', '".sanitize($message)."', '".time()."'".$extraVal.")";
 	$db->prepare($sql)->execute();
 	
 	$db = null;
